6bd6c567ce0ca14d7b94f7794ae5290e3e4a5458
Søren Bruus Frank <soerenbf@gmail.com>
2014-04-29 18:49:04 +0200

    scrolling_nav is now scrolling.

--- scripts/layout.js
+++ scripts/layout.js
@@ -7,18 +7,28 @@
 $(document).ready(function() {
 
 	//Init.
-	resizeElements($(window).width() - $('#nav_scrollbar').width());
 	layoutPrevWorkItems();
 
 	$(window).on('resize', function(event) {
 		//Resize  width of div#prevWork and section#content_area.
 		resizeElements($(window).width() - $('#nav_scrollbar').width());
+		scrollNav();
+	}).on('scroll', function() {
+		scrollNav();
 	});
 });
 
+var data = [];
+
+//Resizing
 var resizeElements = function(elWidth) {
 	$('section#content_area').width(elWidth);
 	$('div#prevWork').width(elWidth);
+
+	//Calculate width of div#prevWork_wrapper based on the amount of data. Every prevWork_item is 640px wide incl. margin. If the width does not exceed windowSize, 
+	var dataWidth = 640 + ((data.length - 1) * 320);
+	var contentWidth = $(window).width() - $('#nav_scrollbar').width();
+	$('div#prevWork_wrapper').width(dataWidth > contentWidth ? dataWidth : contentWidth);
 }
 
 var layoutPrevWorkItems = function() {
@@ -27,14 +37,9 @@ var layoutPrevWorkItems = function() {
 	var pwLower = $('div#prevWork_items_lower');
 
 	//Get portfolio data form database.
-	var dataLength = 4;
+	data = [1, 2, 3, 4];
 
-	//Calculate width of div#prevWork_wrapper based on the amount of data. Every prevWork_item is 640px wide incl. margin. If the width does not exceed windowSize, 
-	var dataWidth = 640 + ((dataLength - 1) * 320);
-	var contentWidth = $(window).width() - $('#nav_scrollbar').width();
-	$('div#prevWork_wrapper').width(dataWidth > contentWidth ? dataWidth : contentWidth);
-
-	for (var i = 0; i < dataLength; i++) {
+	for (var i = 0; i < data.length; i++) {
 		//Depending on 'i', add a new div.prevWork_item to either pwUpper or pwLower.
 		var pwItem = document.createElement('div');
 		var parent = i % 2 === 0 ? pwUpper : pwLower;
@@ -43,4 +48,14 @@ var layoutPrevWorkItems = function() {
 			.appendTo(parent);
 			//Fill in data from database.
 	};
+
+	resizeElements($(window).width() - $('#nav_scrollbar').width());
+}
+
+//Scrolling
+var scrollNav = function() {
+	var pageScrollMax = $('#content_area').height() - $('body').height();
+	var amountScrolled = (1 - (pageScrollMax - window.pageYOffset) / pageScrollMax);
+
+	$('#scrolling_nav').css('top', ($('body').height() - $('#scrolling_nav').height()) * amountScrolled);
 }
