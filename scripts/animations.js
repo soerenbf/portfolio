a4ca094f900062f8eea6d7a5cac213a9facaa3fb
Søren Bruus Frank <soerenbf@gmail.com>
2014-04-30 20:12:18 +0200

    Implmenting the disabling of scrolling when workScroller is activated.

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,5 +1,7 @@
-var workScrollerActive = false;
 var showSignOffset = 1000;
+var showSignInterval = 200;
+
+var workScrollerClicked = 0;
 
 $(document).ready(function() {
 	//Animation for header of prevWork_item elements.
@@ -9,14 +11,33 @@ $(document).ready(function() {
 		$(this).find('prevWork_item_header').animate({bottom: '0px'}, 250);
 	});
 
-	//To show the "click to activate" sign.
-	$(window).on('scroll', function() {
-		if (window.pageYOffset > showSignOffset && window.pageYOffset < showSignOffset + 200 && !workScrollerActive) {
-
+	//To show a hint.
+	$(window).on('scroll', function(event) {
+		if (window.pageYOffset > showSignOffset && window.pageYOffset < showSignOffset + showSignInterval && workScrollerClicked < 2) {
+			switch(workScrollerClicked) {
+				case 0 : //Show "click to activate" hint.
+					break;
+				case 1 : //Show the "scroll to see more" hint.
+					break;
+			}
 		}
+
+		if (workScrollerClicked % 2 === 0) { //It's active
+			
+		} else {
+			
+		};
 	});
 
-	$('work_timeline_scroller').on('click', function() {
-		console.log(1);
+	$('#work_timeline_scroller').on('click', function() {
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$('body').css('overflow', 'auto');
+		} else {
+			$(this).addClass('active');
+			$('body').css('overflow', 'hidden');
+		};
+
+		workScrollerClicked++;
 	});
 });
