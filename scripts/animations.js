97e99b0ffa6d52ac618e3cf19acff8da9e289d89
Søren Bruus Frank <soerenbf@gmail.com>
2014-04-30 16:49:53 +0200

    Started working with interaction with horizontal slider

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,8 +1,22 @@
+var workScrollerActive = false;
+var showSignOffset = 1000;
+
 $(document).ready(function() {
 	//Animation for header of prevWork_item elements.
 	$('div#prevWork_item').mouseenter(function() {
 		$(this).find('prevWork_item_header').animate({bottom: '100px'}, 250);
 	}).mouseleave(function(){
 		$(this).find('prevWork_item_header').animate({bottom: '0px'}, 250);
+	});
+
+	//To show the "click to activate" sign.
+	$(window).on('scroll', function() {
+		if (window.pageYOffset > showSignOffset && window.pageYOffset < showSignOffset + 200 && !workScrollerActive) {
+
+		}
+	});
+
+	$('work_timeline_scroller').on('click', function() {
+		console.log(1);
 	});
 });
