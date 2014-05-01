4aae295f726e75c4460217fb7c4f1b96ed410e79
Søren Bruus Frank <soerenbf@gmail.com>
2014-05-02 00:13:23 +0200

    Deleted obsolete js file.

--- scripts/animations.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	//Animation for header of prevWork_item elements.
-	$('div#prevWork_item').mouseenter(function() {
-		$(this).find('prevWork_item_header').animate({bottom: '100px'}, 250);
-	}).mouseleave(function(){
-		$(this).find('prevWork_item_header').animate({bottom: '0px'}, 250);
-	});
-});
