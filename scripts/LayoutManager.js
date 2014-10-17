3762ef3ccb8042d9e976ca90e5d76b1f6fcb81ff
Søren Bruus Frank <soerenbf@gmail.com>
2014-10-17 11:21:34 +0200

    nothing really....

--- scripts/LayoutManager.js
+++ scripts/LayoutManager.js
@@ -12,7 +12,7 @@ var LayoutManager = (function() {
 				//Depending on 'i', add a new div.prevWork_item to either pwUpper or pwLower.
 				var pwItem = document.createElement('div');
 				var parent = i % 2 === 0 ? pwUpper : pwLower;
-				
+
 				$(pwItem).addClass('prevWork_item')
 					.appendTo(parent);
 					//Fill in data from workItems.
@@ -23,13 +23,13 @@ var LayoutManager = (function() {
 
 			resizeElements($(window).width() - $('#nav_scrollbar').width());
 		}
-		
+
 		var resizeElements = function(elWidth) {
 			//Resize  width of div#prevWork and section#content_area.
 			$('section#content_area').width(elWidth);
 			$('div#prevWork').width(elWidth);
 
-			//Calculate width of div#prevWork_wrapper based on the amount of data. Every prevWork_item is 640px wide incl. margin. If the width does not exceed windowSize, 
+			//Calculate width of div#prevWork_wrapper based on the amount of data. Every prevWork_item is 640px wide incl. margin. If the width does not exceed windowSize,
 			var dataWidth = 640 + ((workArray.length - 1) * 320);
 			var contentWidth = $(window).width() - $('#nav_scrollbar').width();
 			$('div#prevWork_wrapper').width(dataWidth > contentWidth ? dataWidth : contentWidth);
