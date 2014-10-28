7dfaf399cdbf61024aab58d3ad7271b19714ef92
Søren Bruus Frank <soerenbf@gmail.com>
2014-10-28 15:13:28 +0100

    Refactoring

--- scripts/driver.js
+++ scripts/driver.js
@@ -3,17 +3,18 @@ $(document).ready(function() {
 	var backendUrl = '/blog_backend/index.php';
 
 	//Init.
-	window.layoutManager = new LayoutManager(); //LayoutManager.js
+	window.activeCategories = []; //Array to hold categories when filtering is enabled.
 
+	window.layoutManager = new LayoutManager(); //LayoutManager.js
 	window.scrollManager = new ScrollManager(); //ScrollManager.js
-	window.animationManager = new AnimationManager();
+	window.animationManager = new AnimationManager(); //AnimationManager.js
 
 	window.ajaxComm = new AjaxCommunicator(backendUrl); //AjaxCommunicator.js
 
 	//Get the items from the backend.
 	ajaxComm.getPortfolio(layoutManager.layoutPortfolio);
 
-	//Global document wide event handling.
+	/* ---------------- Global document wide event handling. ---------------- */
 	$(window).on('resize', function(event) {
 		layoutManager.updateLayout();
 		scrollManager.handleScroll();
@@ -21,11 +22,13 @@ $(document).ready(function() {
 		scrollManager.handleScroll(event);
 	});
 
-	//Local event handling.
+	/* ---------------- Local event handling. ---------------- */
 	//Show description when hovering over items in the portfolio.
 	$("#prev_work_items_wrapper").on('mouseenter', '.prev_work_item',function() {
 		animationManager.togglePrevWorkItemDescription($(this), true);
 	}).on('mouseleave', '.prev_work_item',function() {
 		animationManager.togglePrevWorkItemDescription($(this), false);
 	});
+
+	//Enable filtering on categories when clicking them.
 });
