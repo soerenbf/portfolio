0333af2aa6053f46e238ca6af9b52eb8274d4e93
Søren Bruus Frank <soerenbf@gmail.com>
2015-01-05 12:39:27 +0100

    fixed scroller bug happening because of dynamic background height.

--- scripts/ScrollingNav.js
+++ scripts/ScrollingNav.js
@@ -2,7 +2,7 @@ var ScrollingNav = (function(scroller) {
   return function() {
 
     var updateScroller = function() {
-      var siteHeight = $('#background').height() + $('#content_area').height();
+      var siteHeight = $(window).height() + $('#content_area').height(); //window.height is included because of scrolling through the #background first, (background height cannot be used as this is dynamic).
       var pageScrollMax = siteHeight - $(window).height();
       var pageAmountScrolled = 1 - (pageScrollMax - window.pageYOffset) / pageScrollMax;
 
